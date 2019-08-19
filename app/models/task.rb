09f2d2673f95f5f4862e1da62b3dfa8c90210a57
Akira <autumn.no7@gmail.com>
2019-08-19 16:26:57 +0900

    検索機能追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
+    scope :sorted, -> { order(created_at: :desc) }
+    scope :expsorted, -> { order(expiration_out: :desc) }
+    scope :wheretitle, -> (params_t){ where("title LIKE ?", "%#{ (params_t) }%") }
+    scope :wherestatus, -> (params_s){ where("status LIKE ?", "%#{ (params_s) }%") }
+    scope :wheres, -> (params_t, params_s){ where("title LIKE ? and status LIKE ?", "%#{ (params_t) }%", "%#{ (params_s) }%") }
 end
