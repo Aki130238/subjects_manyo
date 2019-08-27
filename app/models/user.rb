7348669d7460f9f84eb92b6b64b1a1dc94d63aa3
Akira <autumn.no7@gmail.com>
2019-08-27 19:02:26 +0900

    add:管理者画面追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   has_secure_password
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :new
 end
